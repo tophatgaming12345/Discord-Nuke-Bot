036ad15b3d0db69d0651db79e9fe44cdf1d81224
tophatgaming12345 <tophatgaming12345@gmail.com>
2026-02-22 13:08:18 -0600

    Update author name in TophatNukeBot.py

--- TophatNukeBot.py
+++ TophatNukeBot.py
@@ -1,6 +1,6 @@
 # Disclaimer: This is only for entertainment and educational purposes.  
 # I’m not responsible for what you do with it or any consequences.  
-# Made by Vexi :3
+# Made by tophat
 
 import discord
 from discord.ext import commands
